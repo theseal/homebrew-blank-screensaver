5a5c0a55010e4ffe7c16b5e736a774b1619ae0f8
Johan Carlquist <johan@rymdvarel.se>
2017-08-02 13:36:25 +0200

    Syntax error.

--- Casks/blank-screensaver.rb
+++ Casks/blank-screensaver.rb
@@ -8,7 +8,7 @@ cask 'blank-screensaver' do
 
   screen_saver "macos-blank-screensaver-#{version}/Blank.qtz"
 
-  def caveats; <<-EOF.undent
+  caveats do; <<-EOF.undent
     NOTE: Don't forget to enable the screensaver named "Blank" in "Desktop & Screen saver".
     `open /System/Library/PreferencePanes/DesktopScreenEffectsPref.prefPane`
     EOF
