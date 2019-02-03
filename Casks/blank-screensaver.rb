639e9ad1ddc5066e9856737bb7844b6dc18b75fa
Gustav Ahlberg <Gustav.Ahlberg@gmail.com>
2019-02-03 13:21:24 +0100

    installs blank screen saver

--- Casks/blank-screensaver.rb
+++ Casks/blank-screensaver.rb
@@ -1,17 +1,8 @@
 cask 'blank-screensaver' do
-  version '1.0.0'
-  sha256 '1d1620ed7a06e3c49540b0a18f9925713cd82be940fbb826e1b61af334b6eede'
-
+  version '2.0.0'
+  sha256 'e4ff7ed877f511d4baa45ea9f3446c6238b1b1ed50ce706e1f16729a80dc6093'
   url "https://github.com/theseal/macos-blank-screensaver/archive/#{version}.tar.gz"
   name 'Blank'
   homepage 'https://github.com/theseal/macos-blank-screensaver'
-
-  screen_saver "macos-blank-screensaver-#{version}/Blank.qtz"
-
-  caveats do; <<~EOF
-    NOTE: Don't forget to enable the screensaver named "Blank" in "Desktop & Screen saver".
-    `open /System/Library/PreferencePanes/DesktopScreenEffectsPref.prefPane`
-    EOF
-  end
-
+  screen_saver "macos-blank-screensaver-#{version}/Blank.saver"
 end
