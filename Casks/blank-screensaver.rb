45856692bb91ccab66d2dc82e164108e1cb3913c
Johan Wassberg <johan@rymdvarel.se>
2020-02-17 07:21:29 +0100

    Fix compability with diffrent releases

--- Casks/blank-screensaver.rb
+++ Casks/blank-screensaver.rb
@@ -1,8 +1,33 @@
 cask 'blank-screensaver' do
-  version '2.0.0'
-  sha256 'e4ff7ed877f511d4baa45ea9f3446c6238b1b1ed50ce706e1f16729a80dc6093'
+  version '2.1.0'
+  sha256 '068bafd2707cc9de3cdeedfc8f0d0637b2dbd86c0cb5acb1ff0fec595dd8211d'
+
   url "https://github.com/theseal/macos-blank-screensaver/archive/#{version}.tar.gz"
   name 'Blank'
+
   homepage 'https://github.com/theseal/macos-blank-screensaver'
-  screen_saver "macos-blank-screensaver-#{version}/Blank.saver"
+  screen_saver "macos-blank-screensaver-#{version}/Blank.qtz" if MacOS.version == :catalina
+  screen_saver "macos-blank-screensaver-#{version}/Blank.saver" if MacOS.version == :mojava
+  screen_saver "macos-blank-screensaver-#{version}/Blank.qtz" if MacOS.version == :high_sierra
+  screen_saver "macos-blank-screensaver-#{version}/Blank.qtz" if MacOS.version == :sierra
+
+  caveats <<~EOS
+    NOTE: Don't forget to enable the screensaver named "Blank" in "Desktop & Screen saver".
+    `open /System/Library/PreferencePanes/DesktopScreenEffectsPref.prefPane`
+
+    If you upgraded from an earlier version of Blank you might need to remove
+    the older version by hand.
+    `rm -r ~/Library/Screen\ Savers/Blank.saver`
+  EOS
+
+  caveats <<~EOS
+    NOTE: Don't forget to enable the screensaver named "Blank" in "Desktop & Screen saver".
+    `open /System/Library/PreferencePanes/DesktopScreenEffectsPref.prefPane`
+
+    If you upgraded from an earlier version of Blank you might need to remove
+    the older version by hand…
+    `rm -r ~/Library/Screen\ Savers/Blank.*`
+    …and then reinstall the Cask.
+  EOS
+
 end
